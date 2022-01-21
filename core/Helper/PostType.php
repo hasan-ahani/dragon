5389c24207c2e290ce56308e85c8786e941b23c0
Hasan Ahani <67734902+hasan-ahani@users.noreply.github.com>
2022-01-21 18:16:03 +0330

    fix post type init

--- core/Helper/PostType.php
+++ core/Helper/PostType.php
@@ -41,7 +41,10 @@ abstract class PostType
      */
     public function register()
     {
-        $this->init();
+
+        if (method_exists($this, 'init')){
+            $this->init();
+        }
 
 
         if ($this->type){
@@ -50,15 +53,13 @@ abstract class PostType
     }
 
     /**
-     * init
-     */
-    protected function init() {}
-
-    /**
      * register post type
      */
     public function register_post_type()
     {
+
+        $args = [] ;
+
 
         $defaults = array(
             'public'            => true,
@@ -72,9 +73,25 @@ abstract class PostType
 
         $args = array_merge( $defaults, $this->args );
 
-        if ($this->labels){
-            $args['labels'] = $this->labels;
-        }
+
+        $plural = ucfirst($this->type) . 's';
+        $single = ucfirst($this->type);
+
+        $labels = array(
+            'name'                  => _x( $plural, 'post type general name', DRAGON_I118 ),
+            'singular_name'         => _x( $single, 'post type singular name', DRAGON_I118 ),
+            'add_new'               => _x( 'Add New', $single , DRAGON_I118),
+            'add_new_item'          => __( 'Add New ' . $single, DRAGON_I118 ),
+            'edit_item'             => __( 'Edit ' . $single, DRAGON_I118 ),
+            'new_item'              => __( 'New ' . $single, DRAGON_I118 ),
+            'view_item'             => __( 'View ' . $single , DRAGON_I118),
+            'search_items'          => __( 'Search ' . $plural, DRAGON_I118 ),
+            'not_found'             =>  __( 'No ' . $plural . ' found' , DRAGON_I118),
+            'not_found_in_trash'    => __( 'No ' . $plural . ' found in Trash', DRAGON_I118 )
+        );
+
+        $args['labels'] = array_merge( $labels, $this->labels );
+
 
         register_post_type( $this->type, $args );
     }
