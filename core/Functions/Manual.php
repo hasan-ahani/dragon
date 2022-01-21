2f5934063f89b23ba581c7c422ccf67af35c5e10
Hasan Ahani <67734902+hasan-ahani@users.noreply.github.com>
2022-01-21 17:22:34 +0330

    add dev manual code

---
+++ core/Functions/Manual.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @name        : Manual.php
+ * @version     : 1.0
+ * @author      : Hasan Ahani
+ * @date        : 2022-01-21
+ * @website     : https://wptool.co
+ * @license     : GPLv2 or later https://wptool.co/license
+ */
+defined('ABSPATH') or exit();
+
+class Manual
+{
+
+    public function __construct()
+    {
+        $methods = get_class_methods($this);
+        unset($methods['__construct']);
+
+        foreach ($methods as $method){
+            if (isset($_GET['_m_' . $method])){
+                $this->$method();
+            }
+        }
+    }
+
+}
+
+
+new Manual();
