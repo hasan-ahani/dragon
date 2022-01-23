a9a2b0f5eb49b970b878f0ce40b37b77a2bed348
Hasan Ahani <67734902+hasan-ahani@users.noreply.github.com>
2022-01-24 01:03:22 +0330

    add support meta box

---
+++ core/MetaBox/PageSettings.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Dragon\MetaBox;
+/**
+ * @package     : Dragon\MetaBox
+ * @name        : PageSettings.php
+ * @version     : 1.0
+ * @author      : Hasan Ahani
+ * @date        : 2022-01-23
+ * @website     : https://wptool.co
+ * @license     : GPLv2 or later https://wptool.co/license
+ */
+defined('ABSPATH') or exit();
+
+class PageSettings extends \Dragon\Helper\MetaBox
+{
+
+    public $id = 'PageSettings';
+
+
+    public function getTitle()
+    {
+        return __('tes se');
+    }
+}
---
+++ core/View/MetaBox/page-settings.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @name        : page-settings.php
+ * @version     : 1.0
+ * @author      : Hasan Ahani
+ * @date        : 2022-01-23
+ * @website     : https://wptool.co
+ * @license     : GPLv2 or later https://wptool.co/license
+ */
+defined('ABSPATH') or exit();
+
+?>
+
