389348dd3b5dd3d4b9e387bcd183d049a2d399fa
Hasan Ahani <67734902+hasan-ahani@users.noreply.github.com>
2022-02-24 23:39:59 +0330

    Create DashboardMenus.php

---
+++ core/Utilities/DashboardMenus.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @name        : DashboardMenus.php
+ * @version     : 1.0
+ * @author      : Hasan Ahani
+ * @date        : 2022-02-18
+ * @website     : https://wptool.co
+ * @license     : GPLv2 or later https://wptool.co/license
+ */
+defined('ABSPATH') or exit();
+
+
+return [
+    [
+        'name'      => 'Dashboard',
+        'title'     => __('Dashboard', 'dragon'),
+    ],
+    [
+        'name'      => 'Dashboard',
+        'title'     => __('Dashboard', 'dragon'),
+    ]
+];
