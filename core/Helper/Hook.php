ebd4e72f06fafafd9aa2e3545d53b7d82ed41d41
Hasan Ahani <67734902+hasan-ahani@users.noreply.github.com>
2022-02-24 23:36:23 +0330

    add auto filter & action

--- core/Helper/Hook.php
+++ core/Helper/Hook.php
@@ -13,6 +13,44 @@ defined('ABSPATH') or exit();
 
 abstract Class Hook
 {
+
+
     abstract public function register();
+
+
+    /**
+     * @param $hook_name
+     * @param $callback string|array
+     * @param int $priority
+     * @param int $accepted_args
+     * @return bool
+     */
+    public function filter($hook_name, $callback, $priority = 10, $accepted_args = 1 ): bool
+    {
+        if (!is_array($callback)){
+            $callback = [$this, $callback];
+        }
+
+        add_filter($hook_name, $callback , $priority, $accepted_args);
+        return true;
+    }
+
+
+    /**
+     * @param $hook_name
+     * @param $callback string|array
+     * @param int $priority
+     * @param int $accepted_args
+     * @return bool
+     */
+    public function action($hook_name, $callback, $priority = 10, $accepted_args = 1 ): bool
+    {
+        if (!is_array($callback)){
+            $callback = [$this, $callback];
+        }
+
+        add_action($hook_name, $callback , $priority, $accepted_args);
+        return true;
+    }
 
 }
